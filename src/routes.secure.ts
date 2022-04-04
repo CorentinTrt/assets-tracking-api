ee14d82fb78dec34caca83eb82044b1f4a27f0cd
CoTrt <corentintruffaut@gmail.com>
2022-04-04 10:45:20 -0400

    route.secure.ts - clean swagger blocks + add /transaction endpoints

--- src/routes.secure.ts
+++ src/routes.secure.ts
@@ -5,10 +5,12 @@ import requestValidate from './middlewares/request.validate';
 
 import { createAssetSchema } from './schemas/asset.schema';
 import { createOrderSchema } from './schemas/order.shema';
+import { createTransactionSchema } from './schemas/transaction.schema';
 
 import { deleteUserSessionHandler } from './controllers/session.controller';
 import { createAssetHandler } from './controllers/asset.controller';
 import { createOrderHandler } from './controllers/order.controller';
+import { createTransactionHandler } from './controllers/transaction.controller';
 
 const router = express.Router();
 
@@ -16,55 +18,12 @@ router.get('/check', (req, res) => {
   res.send('Check !');
 });
 
-/**
- * @openapi
- * /s/api/sessions :
- *  delete:
- *    tags:
- *      - Sessions
- *    summary: Close user's current session
- *    Authorization: Bearer <token>
- *    responses:
- *      200:
- *        description: Success
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/DeleteSessionResponse'
- *      403:
- *        $ref: '#/components/responses/ForbiddenError'
- */
 router.delete('/sessions', deleteUserSessionHandler);
 
-/**
- * !! TODO  !!
- * @openapi
- * /s/api/assets :
- *  post:
- *    tags:
- *      - Assets
- *    summary: Create a new asset
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/CreateSessionInput'
- *    Authorization: Bearer <token>
- *    responses:
- *      200:
- *        description: Success
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/DeleteSessionResponse'
- *      403:
- *        $ref: '#/components/responses/ForbiddenError'
- */
 router.post(
   '/assets',
   roleCheck(0),
-  // requestValidate(createAssetSchema),
+  requestValidate(createAssetSchema),
   createAssetHandler
 );
 
@@ -74,5 +33,14 @@ router.post(
   requestValidate(createOrderSchema),
   createOrderHandler
 );
+
+router.post(
+  '/transactions',
+  roleCheck(5),
+  requestValidate(createTransactionSchema),
+  createTransactionHandler
+);
+
+router.get('/transactions', () => {});
 
 export default router;
