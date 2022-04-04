4e6f667ad8ceab7b7fdb1378572f1ae6e9311464
CoTrt <corentintruffaut@gmail.com>
2022-04-04 10:53:24 -0400

    transaction.service - add createTransaction + findAndPopulateTransaction

---
+++ src/services/transaction.service.ts
@@ -0,0 +1,21 @@
+import { FilterQuery } from 'mongoose';
+import TransactionModel, {
+  TransactionDocument,
+} from '../models/transaction.model';
+
+export async function createTransaction(input: TransactionDocument) {
+  try {
+    return await TransactionModel.create(input);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+export async function findAndPopulateTransaction(
+  query: FilterQuery<TransactionDocument>,
+  _collections: Array<string>
+) {
+  const collections = _collections.join(' ');
+
+  return await TransactionModel.findOne(query).populate(collections);
+}
