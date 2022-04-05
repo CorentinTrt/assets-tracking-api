cd220deb2eb4bdb0651624e5c04105af7b944312
CoTrt <corentintruffaut@gmail.com>
2022-04-04 21:25:59 -0400

    transaction.controller - add getTransactionHandler + fix few minor errors

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -3,15 +3,20 @@ import { Request, Response } from 'express';
 import { successResponse, errorResponse } from '../utils/responses';
 import { getTransactionMessages } from '../utils/errors.messages';
 
-import { findExchange } from '../services/exchange.service';
+import { createExchange, findExchanges } from '../services/exchange.service';
 import {
+  createOwnedAsset,
   findAndPopulateOwnedAsset,
   updateOwnedAsset,
 } from '../services/ownedAsset.service';
 import { findAsset } from '../services/asset.service';
-import { createTransaction } from '../services/transaction.service';
+import {
+  createTransaction,
+  findAndPopulateTransactions,
+} from '../services/transaction.service';
 
 import { UserDocument } from '../models/user.model';
+import { execArgv } from 'process';
 
 export async function createTransactionHandler(req: Request, res: Response) {
   const { _id: user }: { _id: UserDocument['_id'] } = res.locals.user.data;
@@ -29,9 +34,10 @@ export async function createTransactionHandler(req: Request, res: Response) {
     date: Date;
   } = req.body;
 
-  const _exchange = await findExchange({ name: exchange.toLowerCase() });
+  const _exchanges = await findExchanges({ name: exchange.toLowerCase() });
 
-  if (!_exchange)
+  if (_exchanges.length === 0)
+    // _response POST transaction
     return res
       .status(400)
       .send(
@@ -40,8 +46,8 @@ export async function createTransactionHandler(req: Request, res: Response) {
         ])
       );
 
-  const getOwnedAssetBought = findAndPopulateOwnedAsset(
-    { user, asset_symbol: assetSymbol, exchange: _exchange._id },
+  const getOwnedAssetBought = await findAndPopulateOwnedAsset(
+    { user, asset_symbol: assetSymbol, exchange: _exchanges[0]._id },
     ['asset']
   );
 
@@ -58,11 +64,11 @@ export async function createTransactionHandler(req: Request, res: Response) {
           user,
           asset: _assetBought._id,
           asset_symbol: assetSymbol,
-          exchange: _exchange._id,
+          exchange: _exchanges[0]._id,
           amount,
         })
       : await updateOwnedAsset(
-          { user, asset_symbol: assetSymbol, exchange: _exchange._id },
+          { user, asset_symbol: assetSymbol, exchange: _exchanges[0]._id },
           { $inc: { amount: _type === 0 ? +amount : -amount } }
         );
   };
@@ -73,7 +79,7 @@ export async function createTransactionHandler(req: Request, res: Response) {
     user,
     asset: _updatedOwnedAssetBought._id,
     type,
-    exchange: _exchange._id,
+    exchange: _exchanges[0]._id,
     amount,
     date,
   };
@@ -82,12 +88,35 @@ export async function createTransactionHandler(req: Request, res: Response) {
     // @ts-ignore
     const transaction = await createTransaction(input);
 
+    // _response POST transaction
     return res.status(201).send(successResponse(201, transaction));
   } catch (error: any) {
+    // _response POST transaction
     return res
       .status(409)
       .send(errorResponse(409, [{ type: 'db', message: error.message }]));
   }
 }
 
-export async function getUserTransactionHandler(req: Request, res: Response) {}
+export async function getUserTransactionsHandler(req: Request, res: Response) {
+  const { _id: user }: { _id: UserDocument['_id'] } = res.locals.user.data;
+
+  // use "+" to split the exchanges in the query
+  const { exchanges: _exchanges } = req.query;
+
+  //   @ts-ignore
+  const exchangeNames = _exchanges?.split(' ');
+  for (let i = 0; i < exchangeNames.length; i++) {
+    exchangeNames[i] = exchangeNames[i].trim();
+    exchangeNames[i] = exchangeNames[i].toLowerCase();
+  }
+
+  const exchange = await findExchanges({ name: exchangeNames });
+
+  const transactions = await findAndPopulateTransactions({ user, exchange }, [
+    'asset',
+  ]);
+
+  // _response DELETE session
+  return res.status(200).send(successResponse(200, transactions));
+}
