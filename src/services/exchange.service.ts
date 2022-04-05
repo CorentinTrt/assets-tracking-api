337ee308513cfab912266569e1b8e08f1d17b8a2
CoTrt <corentintruffaut@gmail.com>
2022-04-04 21:28:07 -0400

    exchange.service - normalize behavior of findExchanges

--- src/services/exchange.service.ts
+++ src/services/exchange.service.ts
@@ -10,10 +10,6 @@ export async function createExchange(input: ExchangeDocument) {
   }
 }
 
-export async function findExchange(query: FilterQuery<ExchangeDocument>) {
-  // DB_call
-  // const asset
-  return (await ExchangeModel.find(query).lean()).length === 1
-    ? (await ExchangeModel.find(query).lean())[0]
-    : await ExchangeModel.find(query).lean();
+export async function findExchanges(query: FilterQuery<ExchangeDocument>) {
+  return await ExchangeModel.find(query).lean();
 }
